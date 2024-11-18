d12e679ee365847bdd7bceed44e97659855e5b17
slackscake <slackscake@gmail.com>
2024-11-18 23:16:11 +0100

    Fixed registration Error

--- main.py
+++ main.py
@@ -2,21 +2,24 @@
 # Создание системы верификации для входа в менеджер паролей
 
 import os
-from verification import check_input
+from verification import UserAuth
 import register as commands
 import genpass as passg
+from verification import UserAuth
 
 entry = commands.Credentials()
 
-
-user_login = input("Username: ")
-user_password = input("Password: ")
+auth = UserAuth()
 
 # эта функция очищяет экран при вызове
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-if check_input(user_login, user_password):
+clear_screen()
+user_login = input("Username: ")
+user_password = input("Password: ")
+
+if auth.check_input(user_login, user_password):
     clear_screen()
     print("To get help type 'help'")
     while True:
--- verification.py
+++ verification.py
@@ -1,33 +1,53 @@
 import bcrypt
 import json
+import os
+from cryptography.fernet import Fernet
 
-try:
-    with open("user.json", "r") as file:
-        crypt_user = json.load(file)
-    str_login = crypt_user["login"]
-    str_password = crypt_user["password"]
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-    bytes_login = str_login.encode()
-    bytes_password = str_password.encode()
+class UserAuth:
+    def __init__(self):
+        self.user_file = "user.json"
+        self.load_user()
 
-    def check_input(input_login, input_password):
-        login_match = bcrypt.checkpw(input_login.encode(), bytes_login)
-        password_match = bcrypt.checkpw(input_password.encode(), bytes_password)
+    def load_user(self):
+        try:
+            with open(self.user_file, "r") as file:
+                crypt_user = json.load(file)
+            self.bytes_login = crypt_user["login"].encode()
+            self.bytes_password = crypt_user["password"].encode()
+        except FileNotFoundError:
+            self.register_new_user()
+
+    def check_input(self, input_login, input_password):
+        login_match = bcrypt.checkpw(input_login.encode(), self.bytes_login)
+        password_match = bcrypt.checkpw(input_password.encode(), self.bytes_password)
         return login_match and password_match
 
-except FileNotFoundError:
-    print("There is not registered user\nPlease enter Login and Password to register a new user")
-    login = input("Login: ")
-    password = input("Password:")
+    def register_new_user(self):
+        clear_screen()
+        print("There is no registered user\nPlease enter Login and Password to register a new user\n")
+        login = input("Login: ")
+        password = input("Password: ")
+        clear_screen()
 
-    hashed_login = bcrypt.hashpw(login.encode(), bcrypt.gensalt())
-    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        hashed_login = bcrypt.hashpw(login.encode(), bcrypt.gensalt())
+        hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
-    hashed_login_str = hashed_login.decode()
-    hashed_password_str = hashed_password.decode()
+        hash_dict = {
+            "login": hashed_login.decode(),
+            "password": hashed_password.decode()
+        }
 
-    hash_dict = {"login" : hashed_login_str, "password" : hashed_password_str}
-    with open("user.json", "w") as file:
-        json.dump(hash_dict, file)
-    input(f"Registration was successful\nRemember Login:{login} Password:{password}\n\nPress any key")
+        with open(self.user_file, "w") as file:
+            json.dump(hash_dict, file)
 
+        print(f"Registration was successful\nRemember your \nLogin:{login} and password:{password}\n\n")
+        input("Press any key to continue")
+
+        self.bytes_login = hashed_login
+        self.bytes_password = hashed_password
+
+# Использование класса
+auth = UserAuth()
